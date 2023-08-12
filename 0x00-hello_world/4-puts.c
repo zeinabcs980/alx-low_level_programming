423d0fc867ea758ca5e527514898f3963153a3b5
zeinabcs980 <zeinabshabban7@gmail.com>
2023-08-12 05:25:05 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
  * Description: A C program that prints with put function
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {       
         puts("\"Programming is like building a multilingual puzzle");
